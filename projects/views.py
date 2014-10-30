f528e0a3fa762e9d018097aa58facfac4bbf00d1
Junyang <junyang@vt.edu>
2014-10-30 13:31:02 -0400

    adding projects index view

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from django.template.response import TemplateResponse
 # Create your views here.
+def index(request):
+    context = { 'active_tag': 'home'}
+    return TemplateResponse(request, 'projects/index.html', context)
+    
+# from django.http import HttpResponse
+# from django.template import RequestContext, loader
+# from django.shortcuts import get_object_or_404, render
+# from django.contrib.auth.forms import AuthenticationForm
+# from django.template.response import TemplateResponse
+
+# def index(request, template_name='home/index.html', authentication_form=AuthenticationForm,):
+    #template = loader.get_template('home/index.html')
+    # form = authentication_form(request)
+    # context = {
+        # 'form': form,
+        # }
+    #return render(request, 'home/index.html')
+    # return TemplateResponse(request, template_name, context)
