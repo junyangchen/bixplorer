026f0499bac8b0707594056b43c89ecee9cf209e
Junyang <junyang@vt.edu>
2014-11-01 17:19:45 -0400

    update

--- bixplorer/settings.py
+++ bixplorer/settings.py
@@ -108,8 +108,8 @@ BASE_URL = "/"
 STATIC_URL = '/static/'
 # Additional locations of static files
 STATICFILES_DIRS = (
-    #get_path('templates'),
-    "E:/xampp/htdocs/bixplorer/static/",
+    get_path('static'),
+    #"E:/xampp/htdocs/bixplorer/static/",
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
