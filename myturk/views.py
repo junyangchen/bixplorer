69639b494a8e8632a799fbadc8035643c211d7d3
Bin He <binhe.whu@gmail.com>
2014-11-04 17:44:40 -0500

    more modules

--- myturk/views.py
+++ myturk/views.py
@@ -4,14 +4,13 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.template.response import TemplateResponse
 from django.http import HttpResponseNotAllowed, HttpResponseForbidden, Http404
 from django.conf import settings
-# from projects.models import Project, Comment, DataSet, Collaborationship
-# from itertools import chain
-# from django.views.decorators.csrf import csrf_protect
-# from django.http import HttpResponse
-# from django.utils import timezone
-# from django.contrib.auth.models import User
-# import json
-# from home.utils import * 
+from itertools import chain
+from django.views.decorators.csrf import csrf_protect
+from django.http import HttpResponse
+from django.utils import timezone
+from django.contrib.auth.models import User
+import json
+from home.utils import * 
 
 
 
