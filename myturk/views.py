33244e5ef4f93eaedf4e3640d6ea5865f489d3c1
Bin He <binhe.whu@gmail.com>
2014-11-04 17:26:18 -0500

    url

--- myturk/views.py
+++ myturk/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def index(request):
+    context = 'Test the response to index page'
+    return TemplateResponse(request, 'projects/index.html', context)
+
+
+def createhit():
+	context = 'Test the response to createhit page'
+    return TemplateResponse(context)
 
