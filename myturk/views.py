ba2357dddda14fd90cf8ff003f70e63dd56248d0
Junyang <junyang@vt.edu>
2014-11-06 13:54:10 -0500

    build myturk strings

--- myturk/views.py
+++ myturk/views.py
@@ -9,6 +9,7 @@ from django.http import HttpResponse
 from django.utils import timezone
 from django.contrib.auth.models import User
 from projects.models import DataSet
+from dataset.models import Doc
 import json
 from home.utils import * 
 
@@ -25,37 +26,51 @@ def index(request):
 #     return TemplateResponse(request, 'myturk/createhit.html', context)
 
 def createhit(request):
-
-
-
-
-
-
-
-
-
-
-
-	
-
-	context = 'Called the createhit method'
-	print('----------------------------')
-	print(context)
-
-	return TemplateResponse(request, 'myturk/createhit.html', context)
-
-
-
-
+    datasets = DataSet.objects.all()   
+    context = { 'active_tag': 'myturk', 'BASE_URL':settings.BASE_URL, 'datasets':datasets}	
+    return TemplateResponse(request, 'myturk/createhit.html', context)
+    
+    
 def createhitsubmit(request):
+    from boto.mturk.connection import MTurkConnection
+    from boto.mturk.question import QuestionContent,Question,QuestionForm,Overview,AnswerSpecification,SelectionAnswer,FormattedContent,FreeTextAnswer
+    import connectMturk as mtconnection
+    print 'Ok'
+    # Get request data from the front-end
+    requestJson = json.loads(request.body)    
+    
+    user_aws_secret_key = requestJson['aws_secret_key']    
+    user_aws_access_key_id = requestJson['aws_access_key_id']
+    task_selected_docs = requestJson['task_selected_docs'] #id
+    task_title = requestJson['task_title']
+    #task_dataset = requestJson['task_dataset'] # id   
+    task_description = requestJson['task_description']    
+        
+    # adjust host setting, depending on whether HIT is live (production) or in testing mode (sandbox)
+    mode = "sandbox"
+    # mode ="production"
 
-	context = 'Called the createhitSSSSubmit method'
+    if mode=="production":
+        HOST='mechanicalturk.amazonaws.com'
+    else:
+        HOST='mechanicalturk.sandbox.amazonaws.com'
 
-	print('----------------------------')
+    mtc = MTurkConnection(aws_access_key_id= user_aws_access_key_id,
+                      aws_secret_access_key= user_aws_secret_key,
+                      host=HOST)
+                      
+    overview = Overview()
+    overview.append_field('Title', task_title)
+    overview.append(FormattedContent('<p>' + task_description + '</p>'))
 
-	context.append(request.body)
-	print(context)
-	return HttpResponse(json.dumps(theresponse), content_type = "application/json")
+    overview.append(FormattedContent('<table>'))
+    for docID in task_selected_docs:
+        docText = Doc.objects.get(pk = docID)
+        overview.append(FormattedContent('<tr><td>' + docText.text + '</td></tr>')) 
+    overview.append(FormattedContent('</table>'))    
+    
+    
+    return HttpResponse(json.dumps({'data' : mtc.get_account_balance():}), content_type = "application/json")
 
 	# return TemplateResponse(request, 'myturk/createhit.html', context)
 
