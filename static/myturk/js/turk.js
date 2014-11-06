5cad2a18c1d225d86dad02c43688543ac4d34e08
Maoyuan <smaoyuan@gmail.com>
2014-11-05 21:24:00 -0500

    update turk front end

--- static/myturk/js/turk.js
+++ static/myturk/js/turk.js
@@ -1,2 +1,39 @@
 
 $('.selectpicker').selectpicker();
+
+$('#btn_submit_turk').click(function(){
+
+
+    var csrftoken = $('#csrf_token').val();
+    var projectID = $('#projectID_token').val();
+
+    var requestJSON = {
+        "project_id": projectID,
+        "collaborator_name": colName
+    } 
+
+    $.ajax({
+        url: window.SERVER_PATH + 'myturk/createhit/',
+        type: "POST",
+        data: JSON.stringify(requestJSON),
+        contentType: "application/json",
+        success: function(data){
+            if(data['status'] == 'success') {
+            	console.log('success!');
+            }
+        },
+        beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        }
+    });
+
+});
+
+
+// these HTTP methods do not require CSRF protection
+function csrfSafeMethod(method) {
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
