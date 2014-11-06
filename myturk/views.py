d3faa6f3df7c70ea191f6f2c9140fae1b8f3f9c8
Bin He <binhe.whu@gmail.com>
2014-11-06 15:57:52 -0500

    Fetch view

--- myturk/views.py
+++ myturk/views.py
@@ -117,53 +117,68 @@ def hitresult(request):
 
 
 def hitresultfetch(request):
+    from boto.mturk.question import QuestionContent,Question,QuestionForm,Overview,AnswerSpecification,SelectionAnswer,FormattedContent,FreeTextAnswer
+
     import mturk_getallreviewablehits as getAllHits
 
-    import connectMturk as mtconnection
-
-    mtc = mtconnection.mtc
+    requestJson = json.loads(request.body)    
+    
+    user_aws_secret_key = requestJson['aws_secret_key']    
+    user_aws_access_key_id = requestJson['aws_access_key_id']
 
     # return TemplateResponse(request, 'myturk/hitResult.html', context)
 
+    # adjust host setting, depending on whether HIT is live (production) or in testing mode (sandbox)
+    mode = "sandbox"
+    # mode ="production"
 
-allHits = getAllHits.get_all_reviewable_hits(mtc)
+    if mode=="production":
+        HOST='mechanicalturk.amazonaws.com'
+    else:
+        HOST='mechanicalturk.sandbox.amazonaws.com'
 
-print('how many hits in allHits')
-print(len(allHits))
-
-## todo get hit content
-print('------before looping through allHits')
-for hit in allHits:
-    assignments=mtc.get_assignments(hit.HITId)
-    print(hit)
-    print(hit.HITId)
-
-    print('assignments in a hit:')
-    print(assignments)
-
-    dir(assignments)
-
-    # print(assignments.Request)
-    # print(assignments.Assignment)
-    # print(assignments.HIT)
-
-
-    for assignment in assignments:
-        print("Answers of the worker %s" % assignment.WorkerId)
-        for question_form_answer in assignment.answers[0]:
-            # for key, value in question_form_answer.fields:
-            #     print "%s: %s" % (key,value)
-            #     print(value)
-            for value in question_form_answer.fields:
-                print(value)
-
-
-        print ("--------------------")
-        
-print('------after looping through allHits')
+    mtc = MTurkConnection(aws_access_key_id= user_aws_access_key_id,
+                      aws_secret_access_key= user_aws_secret_key,
+                      host=HOST)
 
 
 
+
+
+    allHits = getAllHits.get_all_reviewable_hits(mtc)
+
+    print('how many hits in allHits')
+    print(len(allHits))
+
+    ## todo get hit content
+    print('------before looping through allHits')
+    for hit in allHits:
+        assignments=mtc.get_assignments(hit.HITId)
+        print(hit)
+        print(hit.HITId)
+
+        print('assignments in a hit:')
+        print(assignments)
+
+        dir(assignments)
+
+        # print(assignments.Request)
+        # print(assignments.Assignment)
+        # print(assignments.HIT)
+
+        for assignment in assignments:
+            print("Answers of the worker %s" % assignment.WorkerId)
+            for question_form_answer in assignment.answers[0]:
+                # for key, value in question_form_answer.fields:
+                #     print "%s: %s" % (key,value)
+                #     print(value)
+                for value in question_form_answer.fields:
+                    print(value)
+
+
+            print ("--------------------")
+            
+    print('------after looping through allHits')
 
 
     return HttpResponse({'hitresult key':'thevaluefromhitresult'}, content_type = "application/json")
